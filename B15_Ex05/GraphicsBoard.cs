981175201ad58b65e937d27768ca9f8929810f0c
gal <galrettig@gmail.com>
2015-06-15 16:55:15 +0300

    gg

--- B15_Ex05/GraphicsBoard.cs
+++ B15_Ex05/GraphicsBoard.cs
@@ -8,11 +8,31 @@ namespace B15_Ex05
 {
     public class GraphicsBoard : Form 
     {
+        private int m_boardSize;
+        private bool m_multiplayer;
 
-        public GraphicsBoard()
+        public GraphicsBoard(int i_boardSize, bool i_multiplayer)
         {
-            
+            m_boardSize = i_boardSize;
+            m_multiplayer = i_multiplayer;
         }
 
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GraphicsBoard
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "GraphicsBoard";
+            this.Load += new System.EventHandler(this.GraphicsBoard_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void GraphicsBoard_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
